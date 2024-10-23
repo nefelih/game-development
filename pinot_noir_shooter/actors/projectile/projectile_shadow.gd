28376a51573c5d9806c9a749ef5caf699c8ba414
nefeli <89709920+nefelih@users.noreply.github.com>
2024-10-23 09:32:19 -0600

    Delete pinot_noir_shooter_old  directory
    (was pushing two repositories to the same reference)

--- pinot_noir_shooter/actors/projectile/projectile_shadow.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-func _physics_process(delta):
-	rotation = position.direction_to(get_global_mouse_position()).angle()
-	flip_v = (rotation < -PI/2 or rotation > PI/2)
-	await get_tree().create_timer(3).timeout
-
-func _on_timer_timeout() -> void:
-	queue_free()
